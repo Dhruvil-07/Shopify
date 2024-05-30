e73c6e4db7e42f8f3530b59d9c73d8fe77dc6017
Dhruvil Shah <dhruvilshah732@gmail.com>
2024-05-30 19:17:04 +0530

    client-side and server-side set up done

---
+++ client_side/admin_panel/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+   runApp(Shopify_admin());
+}
+
+class Shopify_admin extends StatelessWidget {
+  const Shopify_admin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red,);
+  }
+}
---
+++ client_side/user_interface/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Shopify());
+}
+
+class Shopify extends StatelessWidget {
+  const Shopify({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.black,);
+  }
+}
